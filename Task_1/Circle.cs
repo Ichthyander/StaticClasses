1c4f4942aa447c8394bd1af45a2bf808a65898ab
Ichtyander <di_ignatov@mail.ru>
2022-08-10 14:54:42 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    static class Circle
+    {
+        static public double CircleLength(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+        static public double CircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+        static public string IsBelongs(double pointX, double pointY, double centerX, double centerY, double radius)
+        {
+            //Находим расстояние от центра окружности до точки и сравниваем с радиусом
+            double distanceFromCenter = Math.Sqrt(Math.Pow((pointX - centerX), 2) + Math.Pow((pointY - centerY), 2));
+            if (distanceFromCenter > radius)
+            {
+                return "Точка лежит за пределами окружности";
+            }
+            else if (distanceFromCenter < radius)
+            {
+                return "Точка лежит внутри окружности";
+            }
+            else
+            {
+                return "Точка лежит на окружности";
+            }
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*1.   Разработать статический класс для работы с окружностью. 
+ * Класс должен содержать 3 метода:
+
+метод, определяющий длину окружности по заданному радиусу;
+метод, определяющий площадь круга по заданному радиусу;
+метод, проверяющий принадлежность точки с координатами (x,y) кругу с 
+радиусом r и координатами центра x0, y0.*/
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тестирование программы");
+            Console.WriteLine();
+
+            //Значения для тестирования
+            double radius_1 = 1;
+            double centerX_1 = 0.5;
+            double centerY_1 = 1;
+            double pointX_1 = 2;
+            double pointY_1 = 1;
+            
+            //Блок вывода
+            Console.WriteLine("Длина окружности с радиусом {0} равняется - {1}", radius_1, Circle.CircleLength(radius_1));
+            Console.WriteLine("Площадь окружности с радиусом {0} равняется - {1}", radius_1, Circle.CircleArea(radius_1));
+            Console.WriteLine(Circle.IsBelongs(pointX_1, pointY_1, centerX_1, centerY_1, radius_1));
+            Console.ReadKey();
+        }
+    }
+}
